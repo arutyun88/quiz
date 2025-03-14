edb6eeef8b124e89d9924fa4543d32340f7eadef
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2025-03-14 11:32:41 +0500

    files renaming

--- lib/features/authentication/presentation/sign_in/sign_in_flow.dart
+++ lib/features/authentication/presentation/sign_in/sign_in_flow.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/features/authentication/presentation/sign_in/sign_in_page.dart';
 
 class LoginFlow extends StatelessWidget {
   const LoginFlow({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Log in'),
-      ),
-    );
+    return const LoginPage();
   }
 }
---
+++ lib/features/authentication/presentation/sign_in/sign_in_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Spacer(),
+          Center(
+            child: Text('Log in'),
+          ),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
