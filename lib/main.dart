582515f7046c7b25388d1090d6b85ce59b5ad351
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2025-03-14 12:26:39 +0500

    fix locale app init state

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quiz/app/application.dart';
+import 'package:quiz/app/core/localization/gateway/change_locale_gateway.dart';
 import 'package:quiz/app/core/theme/provider/theme_provider.dart';
 import 'package:quiz/app/di/di.dart';
 import 'package:quiz/gen/strings.g.dart';
@@ -12,6 +13,8 @@ void main() async {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   await configureDependencies();
+
+  await getIt<ChangeLocaleGateway>().reload();
 
   final container = ProviderContainer();
   container.read(themeProvider.notifier).checkCurrentTheme();
