3d0e8e4fa33dc07727a86dd401900a325b460970
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2025-03-30 00:19:16 +0500

    fix remote config initialization

--- lib/app/core/services/firebase_remote_config_service.dart
+++ lib/app/core/services/firebase_remote_config_service.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 
 class FirebaseRemoteConfigService {
@@ -5,7 +8,13 @@ class FirebaseRemoteConfigService {
 
   FirebaseRemoteConfigService(this._remoteConfig);
 
-  Future<void> init() async => await _remoteConfig.fetchAndActivate();
+  Future<void> init() async {
+    try {
+      await _remoteConfig.fetchAndActivate();
+    } on FirebaseException catch (e) {
+      log('Remote Config is not activated: ${e.code}');
+    }
+  }
 
   String fetchByKey(String key) => _remoteConfig.getString(key);
 }
